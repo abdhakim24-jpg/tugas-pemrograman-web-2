308bb4eeccd82f7a2c9371d368b51ca2e9b30378
abdhakim24-jpg <abdhakim24@mhs.unitama.ac.id>
2026-05-12 21:03:08 +0800

    feat: implementasi fungsi hapus data Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,28 +12,47 @@ class CategoryController extends Controller
         $search = $request->search;
 
         $categories = Category::query()
+
             ->when($search, function ($query) use ($search) {
 
                 $query->where('name', 'like', '%' . $search . '%')
+
                       ->orWhere('code', 'like', '%' . $search . '%');
             })
+
             ->paginate(5);
 
         return view('categories.index', compact('categories'));
     }
+
     public function create()
-{
-    return view('categories.create');
-}
-public function edit(Category $category)
-{
-    return view('categories.edit', compact('category'));
-}
+    {
+        return view('categories.create');
+    }
 
-public function store(Request $request)
-{
-    Category::create($request->all());
+    public function store(Request $request)
+    {
+        Category::create($request->all());
 
-    return redirect()->route('categories.index');
-}
+        return redirect()->route('categories.index');
+    }
+
+    public function edit(Category $category)
+    {
+        return view('categories.edit', compact('category'));
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $category->update($request->all());
+
+        return redirect()->route('categories.index');
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('categories.index');
+    }
 }
